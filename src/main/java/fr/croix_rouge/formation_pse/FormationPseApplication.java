2a76ff27eb6b85d49d56be153e47cb0d5721d329
Yachef <yachef.h@gmail.com>
2022-04-24 01:30:32 +0200

    initial commit

---
+++ src/main/java/fr/croix_rouge/formation_pse/FormationPseApplication.java
@@ -0,0 +1,13 @@
+package fr.croix_rouge.formation_pse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationPseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationPseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/croix_rouge/formation_pse/FormationPseApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.croix_rouge.formation_pse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationPseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
