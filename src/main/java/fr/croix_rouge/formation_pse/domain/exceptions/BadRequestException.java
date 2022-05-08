7ad876e30bc7ea8ef82014d0fcbc927ac4621d28
gmonacho <gael.dropix@dropix.fr>
2022-05-08 14:41:57 +0200

    remove spring annotation from domain class BadRequestException

--- src/main/java/fr/croix_rouge/formation_pse/domain/exceptions/BadRequestException.java
+++ src/main/java/fr/croix_rouge/formation_pse/domain/exceptions/BadRequestException.java
@@ -3,7 +3,7 @@ package fr.croix_rouge.formation_pse.domain.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+
 public class BadRequestException extends RuntimeException {
 
   private final String field;
